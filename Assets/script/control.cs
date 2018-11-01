098a3218ae3072f7fb0b6a957ead84a54e6909b6
kazufreak <kazufeak@gmail.com>
2018-11-01 18:12:16 +0900

    test

---
+++ Assets/script/control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+    int speed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            this.speed = 20;
+            for(float i = 0; i < speed; i += 0.001f)
+            {
+                transform.Rotate(0, 0, i);
+            }
+            
+        }
+		
+	}
+}
